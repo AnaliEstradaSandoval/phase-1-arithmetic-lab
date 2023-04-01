eaff3731df43a0311a56684ab79c5b6bca9658f9
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-04-01 12:02:22 -0700

    Arithmetic lab completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31; 
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random()* 10) + 1; 
+
+
+let num3 = 19;
+let num4 = 5;
+
+const mod = num3 % num4;  
+
+let max = Math.max(5,10,15,20);
